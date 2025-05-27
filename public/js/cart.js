0c8d4c06bdd8954c5a667489bb7a79d1c74d60ed
Simon-50 <180413371+Simon-50@users.noreply.github.com>
2025-05-27 10:49:08 +0200

    Add product animation

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,3 +1,31 @@
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function additionAnimation(event) {
+    button = event.target;
+
+    const plus = document.createElement('div');
+    plus.textContent = '+';
+    plus.classList.add('floating-plus');
+
+    const rect = button.getBoundingClientRect();
+
+    // Select random horizontal position within button
+    horisontalPosition = getRandomInt(Math.round(rect.left) + 10, Math.round(rect.right) - 10);
+
+    // Adjust for scroll position
+    plus.style.left = `${horisontalPosition}px`;
+    plus.style.top = `${rect.top + window.scrollY}px`;
+
+    document.body.appendChild(plus);
+
+    // Remove element after animation
+    plus.addEventListener('animationend', () => {
+        plus.remove();
+    });
+}
+
 function updateElement(element, value) {
     element.textContent = value;
 }
@@ -41,9 +69,14 @@ document.addEventListener('click', async (event) => {
         });
 
         if (response.ok) {
-            updateCounter(mealId, 1);
-
             console.log('Added to cart');
+
+            // Update visual indicator if user is on cart page
+            if (location.pathname === '/product') {
+                additionAnimation(event);
+            } else if (location.pathname === '/cart') {
+                updateCounter(mealId, 1);
+            }
         }
     } else if (event.target.matches('.remove-from-cart')) {
         const mealId = event.target.dataset['id'];
@@ -57,9 +90,11 @@ document.addEventListener('click', async (event) => {
         });
 
         if (response.ok) {
-            updateCounter(mealId, -1);
-
             console.log('Removed from cart');
+
+            if (location.pathname === '/cart') {
+                updateCounter(mealId, -1);
+            }
         }
     } else if (event.target.matches('.confirm-order')) {
         const response = await fetch('/cart', {
